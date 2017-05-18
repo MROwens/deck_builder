19a9546e4bdc2953ea4a7a921fcd2ecdfda84680
Matt Owens <Wulfy65@gmail.com>
2017-05-18 02:57:24 -0400

    React state is holding queried cards

--- src/components/CardSearch.js
+++ src/components/CardSearch.js
@@ -7,19 +7,28 @@ class CardSearch extends React.Component{
   constructor(){
     super();
     this.state = {
+      //inital state
       cards: {}
     };
   }
 
-  searchForCards(event){
+//much like add fish | declare state here with cards
+  searchForCards(event, card){
     event.preventDefault();
+    // //existing cards state
+    // const queryCards = {...this.state.queryCards}
+    // //add in new cards
+    // const timestamp = Date.now();
+    // queryCards[`card-${timestamp}`] = card;
+    // //set state
+    // this.setState({queryCards})
+
     mtg.card.where({ name: this.cardInput.value})
       .then(cards => {
           console.log(cards);
+          this.setState({cards});
       })
-    console.log(this.cardInput.value);
-
-
+    //console.log(this.cardInput.value);
   }
 
   render(){
