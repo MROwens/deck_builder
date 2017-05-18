026bc03d077aa3a9b1e577cad4bf90085fe3d35f
Matt Owens <Wulfy65@gmail.com>
2017-05-17 20:23:43 -0400

    Card name search functionality

--- components/CardSearch.js
+++ components/CardSearch.js
@@ -4,7 +4,11 @@ import React from 'react'
 class CardSearch extends React.Component{
 
   searchForCards(){
-
+    mtg.card.all({ name: this.cardInput })
+    .on('data', function (card) {
+        //console.log(card.name, card.text)
+        document.getElementById('cardOutput').innerHTML = "<img src='"+card.imageUrl+"'/>'";
+    });
   }
 
   render(){
@@ -13,8 +17,11 @@ class CardSearch extends React.Component{
       <div className='cardSearch'>
 
         <form className='card-search' onSubmit={this.searchForCards}>
-
+          <input type="text" required placeholder="Card Name" ref={(input) => {this.cardInput = input}} />
+          <button type="submit">Search Cards</button>
         </form>
+
+        <p className="cardOutput"></p>
 
       </div>
 
