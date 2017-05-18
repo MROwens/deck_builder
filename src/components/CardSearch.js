00fd835773b1fa2006cf7c49a4871def24fb4139
Matt Owens <Wulfy65@gmail.com>
2017-05-17 23:06:30 -0400

    Search pulls all like raw card data from API

--- src/components/CardSearch.js
+++ src/components/CardSearch.js
@@ -1,14 +1,40 @@
 import React from 'react'
 
+//const mtg = require('mtgsdk');
+import mtg from 'mtgsdk'
 
 class CardSearch extends React.Component{
+
+  searchForCards(event){
+    event.preventDefault();
+    mtg.card.where({ name: this.cardInput.value})
+      .then(cards => {
+          cards.forEach(showCards)
+
+          function showCards(item){
+          console.log(item.imageUrl)
+        }
+      })
+    console.log(this.cardInput.value);
+
+
+  }
 
   render(){
     return(
 
-      <p>search</p>
+      <div className='cardSearch'>
 
-    )
+        <form className='card-search' onSubmit={(e) => this.searchForCards(e)}>
+          <input type="text" required placeholder="Card Name" ref={(input) => {this.cardInput = input}} />
+          <button type="submit">Search Cards</button>
+        </form>
+
+        <p id="cardOutput"></p>
+
+      </div>
+
+    );
   }
 
 }
